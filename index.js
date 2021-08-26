3317257ea437e96ff2516b50c883419cb6b85496
jeandson <jeandsontb@gmail.com>
2021-08-26 17:22:57 -0300

    vercel file adjustment tree

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const path = require('path');
 const port = process.env.port || 3232;
 
 const app = express();
-const indexRoute = require('./routes/index');
-const tableRoute = require('./routes/tables');
+const indexRoute = require('./src/routes/index');
+const tableRoute = require('./src/routes/tables');
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //====
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRoute);
